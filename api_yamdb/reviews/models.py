2f9bbf3d06d5036479878908bc787a2e127b632e
Максим Кочанов <visperaolnara@gmail.com>
2025-12-08 14:33:40 -0700

    Модели + админка (промежуточный результат)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,43 +1,152 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import (
+    MinValueValidator, MaxValueValidator
+)
+
 
 User = get_user_model()
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(
-        vebrose_name='Название',
         max_length=256,
-        help_text='Выберите категорию'
+        verbose_name='Название категории'
     )
     slug = models.SlugField(
-        vebrose_name='Слаг',
         max_length=56,
         unique=True,
-        help_text='Символы латинского алфавита, цифры и знак подчёркивания'
-
+        verbose_name='Slug категории'
     )
 
     class Meta:
         ordering = ['name']
-        verbose_name = 'Категория'
+        verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
-class Genres(models.Model):
-    pass
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        max_length=56,
+        unique=True,
+        verbose_name='Slug жанра'
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
 
 
-class Titles(models.Model):
-    pass
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения'
+    )
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(1000),
+            MaxValueValidator(2100),
+        ],
+        verbose_name='Год выпуска'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанры'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
 
 
-class Reviews(models.Model):
-    pass
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    score = models.IntegerField(
+        null=True,
+        validators=[
+            MaxValueValidator(10, message='Оценка должна быть не выше 10'),
+            MinValueValidator(1, message='Оценка должна быть не ниже 1')
+        ],
+        verbose_name='Оценка'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        # Обрезаем длинный текст для отображения
+        return f'{self.text[:50]}...' if len(self.text) > 50 else self.text
 
 
-class Comments(models.Model):
-    pass
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'{self.text[:50]}...' if len(self.text) > 50 else self.text
