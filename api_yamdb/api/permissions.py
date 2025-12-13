0c472afe3a6e3b867f744d664186103902dcebd5
Максим Кочанов <visperaolnara@gmail.com>
2025-12-12 20:44:49 -0700

    Правки

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,15 +7,6 @@ class IsAdminUserOrReadOnly(BasePermission):
             request.user.is_authenticated and request.user.is_admin)
 
 
-class IsAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_admin
-            or request.user.is_staff
-            or request.user.is_superuser
-        )
-
-
 class IsAdminModeratorAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
